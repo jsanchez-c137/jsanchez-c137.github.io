9134895e116d5792291fe77dd87d0a966f8ecf08
jsanchezc137 <sanz29052001@gmail.com>
2023-12-07 00:09:16 -0600

    4r commit

---
+++ jsanchezc137/tailwind.config.js
@@ -0,0 +1,13 @@
+import('tailwindcss').Config
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // Agrega cualquier plugin de Tailwind que necesites
+  ],
+}
